501ba80d42a3ecb845c8c1bf44dee3cd9081a41e
Oanh Nguyen <ONguyen@HTMJPN.local>
2023-05-09 16:59:42 +0900

    feat: add images and logos to resources

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+  <header class="bg-[url('@/assets/images/mobile/image-header.jpg')] h-96 border border-indigo-500">
+    <div>
+      <img alt="Vue logo" class="logo" src="@/assets/images/logo.svg" width="125" height="125" />
+    </div>  
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <!-- <HelloWorld msg="You did it!" /> -->
 
-      <nav>
+      <!-- <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-      </nav>
+      </nav> -->
     </div>
   </header>
 
-  <RouterView />
+  <!-- <RouterView /> -->
 </template>
+
+<script setup lang="ts">
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
 
 <style scoped>
 header {
